7cd3f1f29b7c6bc0700cfbccad30dc0eb88a95aa
Yeasir H <yeasir01@gmail.com>
2020-01-16 12:56:16 -0800

    added for loop & clip function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//grab elements from DOM [used const instead of vars so I would not re-declare variables by accident]
+//grab elements from DOM [used const instead of vars - will not be re-declaring variables]
 const resultE = document.getElementById('results');
 const clipE = document.getElementById('clipboard');
 const valueE = document.getElementById('value');
@@ -12,8 +12,18 @@ const generateE = document.getElementById('gen-btn');
 //sets the initial number value in the span based on slider position
 valueE.innerHTML = rangeE.value;
 
+//updates the value in the span based on slider position
 rangeE.oninput = function() {
     valueE.innerHTML = rangeE.value;
+}
+
+//copy password to clipboard
+function copyFunction(){
+    const copyText = resultE;
+    copyText.select();
+    copyText.setSelectionRange(0, 999)
+    document.execCommand('copy');
+    alert("Password Copied: " + copyText.value)
 }
 
 const randomFunction = {
@@ -23,8 +33,7 @@ const randomFunction = {
     symbol: getRandomSymbol
 };
 
-
-//generate event listen
+//generate event listen instead of setting onclick in html document
 generateE.addEventListener('click', () => {
     const length = parseInt(rangeE.value); //returns string, need a number so I parsed value.
     const hasUpper = upperE.checked;
@@ -32,23 +41,43 @@ generateE.addEventListener('click', () => {
     const hasNumber = numberE.checked;
     const hasSymbol = symbolE.checked;
 
-    resultE.innerText = generatePassword(
-        hasUpper, 
-        hasLower, 
-        hasNumber, 
-        hasSymbol, 
-        length)
+    resultE.value = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length); //sets the password value in the textbox
 })
 
-// Genrate password function stopped here
-    function generatePassword(Upper, Lower, Number, Symbol, length) {
+/* 
+GENERATE PASSWORD FUNCTION
+1. Intialize password variable
+2. filter out unchecked types
+3. loop over length call a generator function for each type
+4. Since there are 4 types generator will always return a min of 4 - preform slice
+5. add the final password to the textbox
+*/
 
+function generatePassword(upper, lower, number, symbol, length) {
     let generatedPassword = '';
+    const typesCount = upper + lower + number + symbol; //counts number of values checked
 
-    const typesCount = Upper + Lower + Number + Symbol;
+    const typesArr = [{upper}, {lower}, {number}, {symbol}].filter //set an array, curley brackets return "upper:true" instead of "true"
+    (
+        item => Object.values(item) [0]
+    ); 
+    
+    if(typesCount == 0) { //if all boxes unchecked return an empty string
+        return '';
+    }
 
-    Console.log(typesCount)
+    for (let i = 0; i < length; i += typesCount){ //goes through for loop & generates differnt types
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunction[funcName]();
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
 }
+
 
 /* 
 return String.fromCharCode() [returns a letter, number or symbol from the character set]
@@ -73,4 +102,3 @@ function getRandomSymbol() {
     const symbol = '!@#$%^&*()-_+={}?~';
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
-
